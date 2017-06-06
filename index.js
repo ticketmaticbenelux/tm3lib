78493345338bad0aa948b20e090089db3f6a17e5
Rutger Weemhoff <rutger@direct-control.nl>
2017-06-06 18:30:32 +0200

    Refactoring

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ env(__dirname + '/.env')
 
 const   util = require("util"),
         R = require("ramda"),
+        Promise = require("bluebird"),
         api = require("tm_api")
-
+        
 const client = {
 	shortname: process.env.SHORTNAME,
 	key: process.env.API_KEY,
@@ -15,10 +16,12 @@ const client = {
 
 api.setDebug(true)
 
-const   _ = R.__, apply = R.apply, assoc = R.assoc, curry = R.curry, juxt = R.juxt, pluck = R.pluck, when = R.when, pipe = R.pipe, head = R.head, groupBy = R.groupBy, values = R.values, map = R.map, objOf = R.objOf, prop = R.prop,
+const   _ = R.__, apply = R.apply, assoc = R.assoc, curry = R.curry, juxt = R.juxt, pluck = R.pluck, when = R.when, pipe = R.pipe, head = R.head, groupBy = R.groupBy, values = R.values, objOf = R.objOf, prop = R.prop, tap = R.tap,
+        map = curry(Promise.mapSeries),
         isNotEmpty = R.complement(R.isEmpty),
         query = curry((template, value) => util.format(template, value)),
-        getTickets = pipe(query(`select id from tm.ticket where orderid = %d`), curry(api.query)(client))
+        getTickets = pipe(query(`select id from tm.ticket where orderid = %d`), curry(api.query)(client)),
+        removeContact = curry(api.put)(client, "orders", _, {customerid: null})
 
 const removeTickets = orderid => {
     const apiRemoveTickets = tickets => api.del(client, "tickets", orderid, {tickets})
@@ -27,26 +30,27 @@ const removeTickets = orderid => {
     .then(when(isNotEmpty, apiRemoveTickets))
 }
 
-const removeContact = curry(api.put)(client, "orders", _, {customerid: null})
-
 const cleanOrder = orderid => {
     return removeContact(orderid)
     .then(() => removeTickets(orderid))
     .catch(reason => console.log(reason))
 }
 
+const log = curry((labela, labelb, a, b) => console.log(`${labela}: '${a}' - ${labelb}: '${JSON.stringify(b)}'`))
+
 const getOrderid = pipe(head, prop('orderid'))
 const getTicketPayload = pipe(pluck('ticketid'), objOf('tickets'))
 const apiDelTickets = curry(api.del)(client, 'tickets')
-const cleanTickets = pipe(juxt([getOrderid, getTicketPayload]), apply(apiDelTickets)) 
+const cleanTickets = pipe(juxt([getOrderid, getTicketPayload]), tap(apply(log("Orderid", "Tickets"))), apply(apiDelTickets)) 
 
 const removeTicketsOfEvents = events => {
     const sql = `select o.id orderid, t.id ticketid from tm.order o inner join tm.ticket t on t.orderid = o.id inner join tm.tickettype tt on tt.id = t.tickettypeid where eventid in (${events})`
     return api.export(client, sql)
     .then(groupBy(prop('orderid')))
     .then(values)
-    .then(map(cleanTickets))
+    .then(map(R.__, cleanTickets))
     .catch(reason => console.log(reason))
 }
 
-removeTicketsOfEvents([10000])
+exports.cleanOrder = cleanOrder
+exports.removeTicketsOfEvents = removeTicketsOfEvents
